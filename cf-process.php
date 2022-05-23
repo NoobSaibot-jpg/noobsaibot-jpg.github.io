68144b22144203ff89e89da35dbea874ad6a8f0e
Timofey Man’ko <65870058+NoobSaibot-jpg@users.noreply.github.com>
2022-05-23 21:08:22 +0200

    Update cf-process.php

--- cf-process.php
+++ cf-process.php
@@ -1,38 +1,15 @@
-<?php
+<?
+$to = "kra4kas@gmail.com";
+$subject = $subject;
+$message = $message;
+$from = $email;
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$headers .= "From: <".$from.">\r\n";
 
-$name = $_POST["name"];
-$email = $_POST["kra4kas@gmail.com"];
-$message = $_POST["message"];
-$subject = $_POST["subject"];
-$receiver = $_POST["receiver"];
-$header = "MIME-Version: 1.0\r\n"; 
-$header .= "Content-Type: text/plain; charset=utf-8\r\n"; 
-$header .= "X-Priority: 1\r\n";
-$header .= "From: ${$email}\r\n";
-
-if($name != '' && $email != '' && filter_var($email, FILTER_VALIDATE_EMAIL) && $subject != '' && $message != ''){
-	$body = "";
-	$body .= "Name: ";
-	$body .= $name;
-	$body .= "\n";
-	$body .= "Email: ";
-	$body .= $email;
-	$body .= "\n";
-	$body .= "Subject: ";
-	$body .= $subject;
-	$body .= "\n";
-	$body .= "Message: \n\n";
-	$body .= $message;
-
-	$success = mail($receiver, $subject, $body, $header);
-
-	if ($success){
-		echo 'success';
-	}else{
-		echo 'error';
-	}
-}else{
-	echo 'error';
+if (mail($to,$subject,$message,$headers)) {
+	echo "OK";
 }
-
-?>
+else {
+	echo "ERROR";
+}?>
